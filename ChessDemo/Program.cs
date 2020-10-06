04d2c3dc15e2bc90d1fdbcfa391804ce2ccc315a
Alex Paclin <alex.paclin@gmail.com>
2020-10-06 22:32:42 +0500

    'init'

---
+++ ChessDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using ChessRules;
+using System;
+
+namespace ChessDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chess chess = new Chess(); 
+            while (true)
+            {
+                Console.WriteLine(chess.Fen);
+                
+                var move = Console.ReadLine();
+                if (string.IsNullOrEmpty(move)) break;
+
+                chess.Move(move);
+                
+            }
+        }
+    }
+}
---
+++ ChessRules/Chess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessRules
+{
+    public class Chess
+    {
+        public string Fen { get; private set; }
+
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.Fen = fen;
+        }
+
+        public Chess Move(string move)
+        {
+            return new Chess(this.Fen);
+        }
+
+        public Char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
+    }
+}
